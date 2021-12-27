364e6476949107ff57f07e002a9469642491c75b
Soa <soaornella5@gmail.com>
2021-12-27 20:32:22 +0100

    insertion

--- src/main/java/Entity/Region.java
+++ src/main/java/Entity/Region.java
@@ -1,5 +1,11 @@
 package Entity;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Region {
     /*id int primary key not null AUTO_INCREMENT,
      nom varchar(50) */
@@ -23,6 +29,27 @@ public class Region {
     public Region(int i,String n){
         this.id=i;
         this.nom=n;
+    }
+    public List<Region> select(String req)
+    {
+        List<Region> liste=new ArrayList<>();
+        try
+        {
+            ConnectionBD co=new ConnectionBD();
+            Connection con=co.getConnection();
+            PreparedStatement st=con.prepareStatement(req);
+            ResultSet res=st.executeQuery();
+            while(res.next())
+            {
+                Region reg=new Region(res.getInt("id"),res.getString("nom"));
+                liste.add(reg);
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        return liste;
     }
     public Region(){
         
---
+++ src/main/java/com/example/Test/Controller/AdminController.java
@@ -0,0 +1,34 @@
+package com.example.Test.Controller;
+import java.util.ArrayList;
+import java.util.List;
+import Service.*;
+import Entity.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+@Controller
+public class AdminController {
+    @RequestMapping(value = { "/insertionChefRegion" }, method = RequestMethod.GET)
+    public String insert(Model model) {
+        String page="insertChefRegion.jsp";
+        model.addAttribute("page",page);
+        RegionService r=new RegionService();
+        model.addAttribute("regions",r.getAll());
+        return "templateAdmin";
+    }
+    @RequestMapping(value = { "/ajoutChef" }, method = RequestMethod.GET)
+    public String insert2(@RequestParam(name="nom")String nom,@RequestParam(name="mail")String mail,@RequestParam(name="mdp")String mdp,@RequestParam(name="region")String region,Model model) {
+        String page="insertChefRegion.jsp";
+        model.addAttribute("page",page);
+        RegionService r=new RegionService();
+        ChefRegionService chef=new ChefRegionService();
+        chef.insertChefRegion(nom, mdp, mail, region);
+        model.addAttribute("regions",r.getAll());
+        return "templateAdmin";
+    }
+}
