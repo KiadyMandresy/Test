4c9870957774433c9591e5d26408fb165b279173
Soa <soaornella5@gmail.com>
2022-01-11 06:37:17 +0100

    connection

--- src/main/java/Entity/ConnectionBD.java
+++ src/main/java/Entity/ConnectionBD.java
@@ -10,9 +10,9 @@ public class ConnectionBD {
         Connection con=null;
         try{
             Class.forName("org.postgresql.Driver");
-            String dbURL = "jdbc:postgresql://ec2-34-255-225-151.eu-west-1.compute.amazonaws.com:5432/dbcncf16cfm072";
-            String user = "ocvjbxjbojswhc";
-            String pass="6ae926723aa756a690db2c0f40e82821527714e60f7cd067fced8872e74976ba";
+            String dbURL = "jdbc:postgresql://localhost:5432/final";
+            String user = "postgres";
+            String pass="kalisi61";
             con = DriverManager.getConnection(dbURL, user, pass);
         }catch(Exception e){
             e.printStackTrace();
