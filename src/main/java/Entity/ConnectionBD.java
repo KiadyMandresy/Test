b080db09ecd571ac23cf7cbd346030fff2a53411
kiady <kiadyrandria07@gmail.com>
2022-02-26 21:08:04 +0300

    v5

--- src/main/java/Entity/ConnectionBD.java
+++ src/main/java/Entity/ConnectionBD.java
@@ -11,7 +11,7 @@ public class ConnectionBD {
         try{
 
             Class.forName("org.postgresql.Driver");
-            String dbURL = "jdbc:postgresql://ec2-54-220-243-77.eu-west-1.compute.amazonaws.com:5432/dcjcs7q252ejoq?password=72fda7a47790d34124da0be6e4d1f8efd31623feecbd327651dbf820c61b6a0e&sslmode=require&user=eoachhsokrigti";
+            String dbURL = "jdbc:postgresql://ec2-54-220-243-77.eu-west-1.compute.amazonaws.com:5432/dcjcs7q252ejoq";
             String user = "eoachhsokrigti";
             String pass="72fda7a47790d34124da0be6e4d1f8efd31623feecbd327651dbf820c61b6a0e";
             
