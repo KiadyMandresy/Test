989e79c9a9a1eaac4a4a0bc45d77e4c7d0842544
kiady <kiadyrandria07@gmail.com>
2022-02-27 22:20:12 +0300

    lol

--- src/main/java/Entity/ConnectionBD.java
+++ src/main/java/Entity/ConnectionBD.java
@@ -13,7 +13,6 @@ public class ConnectionBD {
             String dbURL = "jdbc:postgresql://ec2-54-220-243-77.eu-west-1.compute.amazonaws.com:5432/dcjcs7q252ejoq";
             String user = "eoachhsokrigti";
             String pass="72fda7a47790d34124da0be6e4d1f8efd31623feecbd327651dbf820c61b6a0e";
-            
 
             
             con = DriverManager.getConnection(dbURL, user, pass);
