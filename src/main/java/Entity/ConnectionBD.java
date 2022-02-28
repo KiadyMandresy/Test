3f39aa1b7bce0dfc761f0714c5ac165b0d65a6cc
kiady <kiadyrandria07@gmail.com>
2022-02-28 05:07:48 +0300

    i

--- src/main/java/Entity/ConnectionBD.java
+++ src/main/java/Entity/ConnectionBD.java
@@ -10,10 +10,11 @@ public class ConnectionBD {
         Connection con=null;
         try{
             Class.forName("org.postgresql.Driver");
-            String dbURL = "jdbc:postgresql://localhost:5432/final";
-            String user = "postgres";
-            String pass="kalisi61";
+            String dbURL = "jdbc:postgresql://ec2-54-220-243-77.eu-west-1.compute.amazonaws.com:5432/dcjcs7q252ejoq";
+            String user = "eoachhsokrigti";
+            String pass="72fda7a47790d34124da0be6e4d1f8efd31623feecbd327651dbf820c61b6a0e";
             con = DriverManager.getConnection(dbURL, user, pass);
+            
         }catch(Exception e){
             e.printStackTrace();
         }
